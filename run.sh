8ad3a5f1f8e4a30092d97648d27f3148c11dfe21
Daniel Middleton <me@daniel-middleton.com>
2015-08-23 17:02:33 +0100

    run.sh checks for Ansible.

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+if ! hash ansible 2>/dev/null; then
+    echo -e "\nAnsible is not installed. See: http://docs.ansible.com/ansible/intro_installation.html\n"
+    exit 1
+fi
+
 ansible-playbook -i hosts -u root master.yml
